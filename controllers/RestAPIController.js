bca5902b315cc643f27b65b54ee3858db8e14fcb
Ajay <vergilssj@gmail.com>
2016-07-23 06:10:42 +0800

    changed update operation

--- controllers/RestAPIController.js
+++ controllers/RestAPIController.js
@@ -25,10 +25,12 @@ else
 };
 
 exports.update = function(req, res) {
-  var key = req.params._key;
+ var key = req.body._key;
   var updates = req.body;
 updates.timestamp=Math.floor((new Date()).getTime() / 1000);
-  values.findOneAndUpdate({"_key":key},req.body, {sort: { 'timestamp' : -1 },upsert:true,setDefaultsOnInsert:true},
+
+
+  values.findOneAndUpdate({'_key':key,'timestamp':updates.timestamp},req.body,{sort: { 'timestamp' : -1 },upsert:true,setDefaultsOnInsert:true},
     function (err, updateddata) {
       if (err) return console.log(err);
       console.log(updateddata);
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ module.exports = function(app){
 
     app.get('/object', vaultdragon.findAll);
     app.get('/object/:_key', vaultdragon.findByKey);
-    app.post('/object/:_key', vaultdragon.update);
+    app.post('/object/', vaultdragon.update);
     app.post('/object/:_key', vaultdragon.delete);
 	
 	//Testfunction to populate data
